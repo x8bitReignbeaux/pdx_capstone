4db205c6f77fbf1e2c622e688aafa334d9120d7a
x8bitReignbeaux <zachary.christman@8brconsulting.dev>
2022-10-27 00:35:16 -0400

    Setting up the remainder of the Django project and template files.

--- Hook/settings.py
+++ Hook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Hook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Ethnicity(models.Model):
+    ethnicity = models.CharField(max_length=25)
+    isActive = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.ethnicity
